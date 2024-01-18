b1ef1ffb9e1d49a0dea93328d9a2cc0da8c71b16
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2024-01-18 21:18:00 +0200

    news-api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Spinner } from 'react-bootstrap';
+import { Button, Card, Col, Row, Spinner } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './components/News/News';
 
 
 function App() {
+  const [news, setNews] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://newsapi.org/v2/everything?q=tesla&from=2023-12-18&sortBy=publishedAt&apiKey=e19659c585ee4f93a0c61fc918ab1716")
+      .then(res => res.json())
+      .then(data => setNews(data.articles));
+
+  }, []);
+
   return (
     <div className="App">
-      <Button variant="primary">Hello me</Button>
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
+      <Row xs={1} md={2} className="g-4">
+      {
+        news.map(nws => <News news={nws}></News>)
+      }
+    </Row>
+      
     </div>
   );
 }
---
+++ src/components/News/News.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+
+const News = (props) => {
+  console.log(props.news);
+  const {title, description, urlToImage} = props.news;
+    return (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={urlToImage} />
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>
+                {description}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+    );
+};
+
+export default News;
